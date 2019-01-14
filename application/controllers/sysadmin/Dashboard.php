e46f8e8a501bdfd814d4718f01dfeae5b6ee3902
erikwibowo <erikwibo@gmail.com>
2019-01-14 16:08:09 +0700

    setting awal project

---
+++ application/controllers/sysadmin/Dashboard.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Dashboard extends CI_Controller {
+
+	public function index(){
+		$data['title']		= "Dashboard - Bubak Smart Dusun";
+		$data['subtitle']	= "Dashboard";
+		$data['dashboard']	= "active";
+
+		$this->load->view('sysadmin/index', $data);
+	}
+
+}
+
+/* End of file Dashboard.php */
+/* Location: ./application/controllers/sysadmin/Dashboard.php */
